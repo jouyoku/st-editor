b944c3146d5575e23195fa7e8dc0b7ec6d276a3f
Cid Huang <cid@msbt.com.tw>
2022-07-23 15:54:29 +0800

    add: arc and text. reduce bundle size.

--- src/SystemMap/SystemMap.tsx
+++ src/SystemMap/SystemMap.tsx
@@ -1,7 +1,10 @@
 import { Component, onMount, createSignal } from 'solid-js';
-import { Application, Graphics } from 'pixi.js'
+import { Application, Graphics, Text } from './pixi.js'
+//import {  } from '@pixi/graphics'
+//import { Text } from '@pixi/text'
 
-const SystemMap: Component<{id: string}> = (props) => {
+const SystemMap: Component<{ id: string }> = (props) => {
+    const [offset, setOffset] = createSignal({ x: 0, y: 0 });
     const [pos, setPos] = createSignal({ x: 0, y: 0 });
     const [pos0, setPos0] = createSignal({ x: 20, y: 50 });
     const [pos1, setPos1] = createSignal({ x: 20, y: 20 });
@@ -9,38 +12,59 @@ const SystemMap: Component<{id: string}> = (props) => {
     const [app, setApp] = createSignal(null);
 
     onMount(async () => {
+        const canvas = document.getElementById(props.id) as HTMLCanvasElement;
         const app0 = new Application({
-            view: document.getElementById(props.id) as HTMLCanvasElement,
+            view: canvas,
             resolution: window.devicePixelRatio || 1,
             autoDensity: true,
             backgroundColor: 0xffff80,
             width: 640,
             height: 480
         });
+        const rect = canvas.getBoundingClientRect();
+        setOffset({x: rect.x, y: rect.y});
 
         setApp(app0);
     });
 
     function handleMouseMove(event) {
         setPos1({
-            x: event.clientX,
-            y: event.clientY
-        });
-
-    }
-
-    function handleMouseDown(event) {
-        setPos1({
-            x: event.clientX,
-            y: event.clientY
+            x: event.clientX - offset().x,
+            y: event.clientY - offset().y
         });
 
         let curve = new Graphics();
         curve.lineStyle(2, 0xff0000);
-        curve.moveTo(pos0().x, pos0().y);
-        curve.quadraticCurveTo((pos0().x + pos1().x) / 1, (pos0().y + pos1().y) / 1, pos1().x, pos1().y);
+        curve.arc(pos1().x, pos1().y, 100, 0, 2 * Math.PI);
         app().stage.removeChildren();
         app().stage.addChild(curve);
+
+        
+        let text = new Text('bala');
+        text.x = pos1().x;
+        text.y = pos1().y;
+        app().stage.addChild(text);
+    }
+
+    function handleMouseDown(event) {
+        setPos1({
+            x: event.clientX - offset().x,
+            y: event.clientY - offset().y
+        });
+
+
+        let curve = new Graphics();
+        curve.lineStyle(2, 0xff0000);
+        curve.arc(pos1().x, pos1().y, 100, 0, 2 * Math.PI);
+        app().stage.removeChildren();
+        app().stage.addChild(curve);
+
+        
+        let text = new Text('bala');
+        text.x = pos1().x;
+        text.y = pos1().y;
+        app().stage.addChild(text);
+        
     }
 
     return (
